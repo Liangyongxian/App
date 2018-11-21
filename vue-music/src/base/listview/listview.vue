35611eb50420305f368424b406358575fd6bb20b
lyx <745408092@qq.com>
2018-11-21 21:52:18 +0800

    歌手页面-左侧栏索引与右侧列表的滚动关系

--- vue-music/src/base/listview/listview.vue
+++ vue-music/src/base/listview/listview.vue
@@ -1,7 +1,7 @@
 <template>
-  <scroll class="listview" :data="data">
+  <scroll class="listview" :data="data" ref="listview">
     <ul>
-      <li v-for="group in data" class="list-group">
+      <li v-for="group in data" class="list-group" ref="listGroup">
         <h2 class="list-group-title">{{group.title}}</h2>
         <ul>
           <li v-for="item in group.items" class="list-group-item">
@@ -11,18 +11,64 @@
         </ul>
       </li>
     </ul>
+    <div class="list-shortcut" @touchstart="onShortcutTouchStart"
+         @touchmove.stop.prevent="onShortcutTouchMove">
+      <ul>
+        <li v-for="(item, index) in shortCutList" class="item"
+            :data-index="index">
+          {{item}}
+        </li>
+      </ul>
+    </div>
+    <div>
+
+    </div>
   </scroll>
 </template>
 
 <script>
   import Scroll from 'base/scroll/scroll'
+  import {getData} from 'common/js/dom'
+
+  const ANCHOR_HEIGHT = 18
 
   export default {
     name: "listview",
+    created() {
+      this.touch = {}
+    },
     props: {
       data: {
         type: Array,
         default: []
+      }
+    },
+    computed: {
+      shortCutList() {
+        return this.data.map((group) => {
+          return group.title.substr(0, 1)
+        })
+      }
+    },
+    methods: {
+      onShortcutTouchStart(e) {
+        /* eslint-disable */
+        /* anchorIndex获取左侧边栏的字母索引，然后scrollToElement转到相应的左侧字母块歌单中 */
+        let anchorIndex = getData(e.target, 'index')
+        let firstTouch = e.touches[0]
+        this.touch.y1 = firstTouch.pageY
+        this.touch.anchorIndex = anchorIndex
+        this._scrollTo(anchorIndex)
+      },
+      onShortcutTouchMove(e) {
+        let firstTouch = e.touches[0]
+        this.touch.y2 = firstTouch.pageY
+        let delta = (this.touch.y2 - this.touch.y1) / ANCHOR_HEIGHT | 0
+        let anchorIndex = parseInt(this.touch.anchorIndex) + delta
+        this._scrollTo(anchorIndex)
+      },
+      _scrollTo(index) {
+        this.$refs.listview.scrollToElement(this.$refs.listGroup[index], 0)
       }
     },
     components: {
@@ -32,7 +78,7 @@
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
- @import "~common/stylus/variable"
+  @import "~common/stylus/variable"
   .listview
     position: relative
     width: 100%
@@ -60,4 +106,40 @@
           margin-left: 20px
           color: $color-text-l
           font-size: $font-size-medium
+    .list-shortcut
+      position: absolute
+      z-index: 30
+      right: 0
+      top: 50%
+      transform: translateY(-50%)
+      width: 20px
+      padding: 20px 0
+      border-radius: 10px
+      text-align: center
+      background: $color-background-d
+      font-family: Helvetica
+      .item
+        padding: 3px
+        line-height: 1
+        color: $color-text-l
+        font-size: $font-size-small
+        &.current
+          color: $color-theme
+    .list-fixed
+      position: absolute
+      top: 0
+      left: 0
+      width: 100%
+      .fixed-title
+        height: 30px
+        line-height: 30px
+        padding-left: 20px
+        font-size: $font-size-small
+        color: $color-text-l
+        background: $color-highlight-background
+    .loading-container
+      position: absolute
+      width: 100%
+      top: 50%
+      transform: translateY(-50%)
 </style>
--- vue-music/src/common/js/dom.js
+++ vue-music/src/common/js/dom.js
@@ -11,3 +11,17 @@ export function addClass(el, className) {
   newClass.push(className)
   el.className = newClass.join(' ')
 }
+
+export function getData(el, name, val) {
+  let prefix = 'data-'
+  name = prefix + name
+  if (val) {
+    return el.setAttribute(name, val)
+  } else {
+    /* eslint-disable */
+   /*  获取下面元素中的data-index对应的5
+    <li data-v-5e19b76a="" data-v-32f276ac="" data-index="5" class="item"> F </li>
+   */
+   return el.getAttribute(name)
+  }
+}
